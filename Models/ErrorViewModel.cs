e234f611803d82e7a20a65de67fbcf9c526238ba
KoYoungSuk <vheh5678@naver.com>
2021-04-13 14:05:26 +0900

    Initial Comment

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _20210413.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
